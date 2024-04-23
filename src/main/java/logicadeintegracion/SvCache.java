ea1054560f8c8b7ed5870fc57a4a9914ecfee009
ArielGC00 <josgomez@estudiantec.cr>
2024-04-22 20:30:02 -0600

    Borra los textos cuando se da click a volver

---
+++ src/main/java/logicadeintegracion/SvCache.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package logicadeintegracion;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author josea
+ */
+@WebServlet(name = "SvCache", urlPatterns = {"/SvCache"})
+public class SvCache extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        String  textosMostrados = null;
+        String descripcionTexto=null;
+        session.setAttribute("textosMostrados", textosMostrados);
+        session.setAttribute("descripcionTextoCompleto", descripcionTexto);
+        response.sendRedirect("registro_exitoso.jsp");
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
